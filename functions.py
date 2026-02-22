2ea0d6d64e21a0ce12e40ee2dbe9d3638a7756c5
AntonYurkovskiy <antonyurkovskiy@yandex.ru>
2026-02-22 07:53:47 +0200

    personal_id added

--- functions.py
+++ functions.py
@@ -408,3 +408,12 @@ def only_letters_digits_spaces(text):
     return bool(re.match(pattern, text.strip()))
 
 
+def get_personal_id_by_passport(pasports_list_of_dicts):
+    priority_docs = ['International passport', 'National passport', "Seaman's book"]
+    
+    for doc_type in priority_docs:
+        for doc in (pasports_list_of_dicts or []):
+            if doc.get('Title of document') == doc_type:
+                return doc['No.'] if only_letters_digits_spaces(doc['No.']) else None, doc_type
+    
+    return None, 'No documents'
