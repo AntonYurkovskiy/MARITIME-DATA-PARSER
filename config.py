12ad952914251fbc3df26ebc30a8624911598ea5
AntonYurkovskiy <antonyurkovskiy@yandex.ru>
2026-05-28 16:18:43 +0200

    config.py added

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_BASE_URL = os.getenv("API_BASE_URL", "https://staffdev.360crewing.com/api/v1")
+API_TIMEOUT = (30, 60)
+
+INPUT_DIR = os.getenv("INPUT_DIR", "out/out_min")
+FUZZY_THRESHOLD = 80
